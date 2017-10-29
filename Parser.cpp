bb8b9d5aee9906d47d448afcbc4eb2a020b314f7
lt5ed <lt5ed@turing2.richmond.edu>
2017-10-29 14:45:09 -0400

    Updating Errors

--- Parser.cpp
+++ Parser.cpp
@@ -4,9 +4,9 @@ Parser::Parser(string filename){
 
 fstream fs;
 fs.open(filename);
-  if(!file.find(".txt")){
+  if(!filename.find(".config")){
     cerr<<"CONFIGURATION FILE NEEDS TO BE A .txt FILE";
-    system.exit();
+    system.exit(1);
   }
   string line;
  while(getline(fs,line)){
@@ -16,35 +16,44 @@ fs.open(filename);
    }
    else{
      while(line.substr(0,1)==" "){
-       line = line.substr(1,line.length);
+       line = line.substr(1,string::npos;
      }       
-     switch(line.sub(0,8){
+     switch(line.substr(0,8)){
        case "program_": setProgramInput(line.substr(line.find("=")+1,string::npos));
-             break;
+                        break;
+
        case "memory_c": setMemoryInput(line.substr(line.find("=")+1,string::npos));
-            break;
+                        break;
+
        case "register": setRegisterInput(line.substr(line.find("=")+1,string::npos));
-            break;
+                        break;
+
        case "output_m": setOutputMode(line.substr(line.find("=")+1,string::npos));
-            break;
+                        break;
+
        case "debug_mo": if(line.find("true")){
                               setDebugMode(true);
                         }
                         else {setDebugMode(false);}
-            break;
+                        break;
+
        case "print_me":  if(line.find("true")){
                               setPrintContents(true);
                         }
                         else {setPrintContents(false);}
-            break;
+                        break;
+
        case "output_f": setOutputFile(line.substr(line.find("=")+1,string::npos));
-            break;
+                        break;
+
        case "write_to":  if(line.find("true")){
                               setWritetoFile(true);
                         }
                         else {setWriteToFile(false);}
+                        break;
      }   
  }
+}
 
 fs.close()
 }
