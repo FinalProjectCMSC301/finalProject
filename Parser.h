236e43f33dd5730bab5ec654c81fba10b5172184
laurentoth <lauren.toth@richmond.edu>
2017-10-29 13:22:33 -0400

    Update Parser.h

--- Parser.h
+++ Parser.h
@@ -3,6 +3,7 @@
 
 using namespace std;
 #include <fstream>
+#include <iostream>
 
 //This Class will parse the configuration file to set up the program. The correct file format must be given.
 
@@ -10,5 +11,35 @@ class Parser{
 
   public:
    Parser(string filename);
+
+  private:
+	string program_input;
+	string memory_contents_input;
+	string register_file_input;
+	string output_mode;
+	bool debug_mode;
+	bool print_memory_contents;
+	bool write_to_file;
+	string output_file;
+
+
+
+   setProgramInput(string input);
+   setMemoryInput(string memory);
+   setRegisterInput(string registerInput);
+   setOutputMode(string outputMode);
+   setDebugMode(bool debugMode);
+   setPrintContents(bool print);
+   setWriteToFile(bool writeToFile);
+   setOutputFile(string outputFile);
+
+   string getProgramInput();
+   string getMemoryInput();
+   string getRegisterInput();
+   string getOutputMode();
+   bool getDebugMode();
+   bool getPrintContents();
+   bool etWriteToFile();
+   string getOutputFile();
  };
  #endif
