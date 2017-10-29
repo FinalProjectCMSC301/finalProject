b437a08248573856314de95de5d84cef51d19eaf
laurentoth <lauren.toth@richmond.edu>
2017-10-29 12:24:42 -0400

    Create Parser.h

---
+++ Parser.h
@@ -0,0 +1,14 @@
+#ifndef __PARSER_H__
+#define __PARSER_H__
+
+using namespace std;
+#include <fstream>
+
+//This Class will parse the configuration file to set up the program. The correct file format must be given.
+
+class Parser{
+
+  public:
+   Parser(string filename);
+ };
+ #endif
