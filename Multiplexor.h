4f936884d6de45da54829033879110351b127a3f
laurentoth <lauren.toth@richmond.edu>
2017-12-05 13:49:08 -0500

    Added debug

--- Multiplexor.h
+++ Multiplexor.h
@@ -1,3 +1,9 @@
+/*
+This class represents a multiplexor. 
+Input: two values to choose from, and the choice int
+Output: The value that was chosen
+*/
+
 #ifndef __MULTIPLEXOR_H__
 #define __MULTIPLEXOR_H__
 
@@ -8,6 +14,8 @@ public:
 string Multiplexor(string input1, string input2, int choice);
 int Multiplexor(int input1, int input2, int choice);
 string Multiplexor(string input1, int input2, int choice);
+  bool debug = false;
+  void setDebug(bool debugChange){debug=debugChange;};
 
 };
 #endif
