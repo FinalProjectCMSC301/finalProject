dd5f58a1fed206a76f6ed87ea88524215e3e2134
laurentoth <lauren.toth@richmond.edu>
2017-12-05 16:00:49 -0500

    Update Multiplexer.h

--- Multiplexer.h
+++ Multiplexer.h
@@ -7,13 +7,13 @@ Output: The value that was chosen
 #ifndef __MULTIPLEXER_H__
 #define __MULTIPLEXER_H__
 
-class Multiplexor{
+class Multiplexer{
 
 
 public:
-string Multiplexor(string input1, string input2, int choice);
-int Multiplexor(int input1, int input2, int choice);
-string Multiplexor(string input1, int input2, int choice);
+string useMultiplexer(string input1, string input2, int choice);
+int useMultiplexer(int input1, int input2, int choice);
+string useMultiplexer(string input1, int input2, int choice);
   bool debug = false;
   void setDebug(bool debugChange){debug=debugChange;};
 
