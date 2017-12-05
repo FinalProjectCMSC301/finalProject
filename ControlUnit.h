882feb3905a4aa7436b971c101793f48c6dd13cd
laurentoth <lauren.toth@richmond.edu>
2017-12-05 13:28:47 -0500

    Update ControlUnit.h

--- ControlUnit.h
+++ ControlUnit.h
@@ -36,7 +36,7 @@ private:
 	string getMemRead(){return memRead;};
 	string getMemtoReg(){return memtoReg;};
 	string getALUOp1(){return ALUOp1;};
-	string getALUOp2(){return ALUOp2;};
+	string getALUOp0(){return ALUOp0;};
 	string getMemWrite(){return memWrite;};
 	string getALUSrc(){return ALUSrc;};
 	string getRegWrite(){return regWrite;};
@@ -47,11 +47,11 @@ private:
 	string setMemRead(string mem){ memRead = mem;};
 	string setMemtoReg(string mem){ memtoReg = mem;};
 	string setALUOp1(string alu){ ALUOp1 = alu;};
-	string setALUOp2(string alu){ ALUOp2 = alu;};
+	string setALUOp0(string alu){ ALUOp0 = alu;};
 	string setMemWrite(string mem){ memWrite = mem;};
 	string setALUSrc(string alu){ ALUSrc = alu;};
 	string setRegWrite(string reg){ regWrite = reg;};
-	void printValues(){cout << "RegDST: " << regDst << "\n" << "Branch: " << branch << "\n" << "Jump: " << jump << "\n" << "MemRead: " << memRead << "\n" << "MemtoReg: " << memtoReg << "\n" << "ALUOP1: " << ALUOp1 << "\n" << "ALUOp2: " << ALUOp2 << "\n" << "ALUSrc: " << ALUSrc << "\n" << "MemWrite: " << memWrite << "\n" << "RegWrite: " << regWrite << "\n";};
+	void printValues(){cout << "RegDST: " << regDst << "\n" << "Branch: " << branch << "\n" << "Jump: " << jump << "\n" << "MemRead: " << memRead << "\n" << "MemtoReg: " << memtoReg << "\n" << "ALUOP1: " << ALUOp1 << "\n" << "ALUOp0: " << ALUOp0 << "\n" << "ALUSrc: " << ALUSrc << "\n" << "MemWrite: " << memWrite << "\n" << "RegWrite: " << regWrite << "\n";};
 	
 
 
