1af8cc0c2d1201e8281eb98b00f937d29d271773
laurentoth <lauren.toth@richmond.edu>
2017-12-05 16:02:07 -0500

    Update Multiplexer.cpp

--- Multiplexer.cpp
+++ Multiplexer.cpp
@@ -2,9 +2,12 @@
 #include <string>
 #include <sstream>
 
-string Multiplexor::Multiplexor(string input1, string input2, int choice){
+Multiplexer::Mutiplexer(){
+}
+
+string Multiplexer::useMultiplexer(string input1, string input2, int choice){
 	if (debug){
-		cout << "*** MULTIPLEXOR STRING/STRING***" << "\n" << "String1: " << input1 << " String2: " << input2 << "Choice: " << choice << "\n";
+		cout << "*** MULTIPLEXER STRING/STRING***" << "\n" << "String1: " << input1 << " String2: " << input2 << "Choice: " << choice << "\n";
 	}
 	
 	cout << "Input: " << input1 << " " << input2 << "\n"; 
@@ -21,10 +24,10 @@ string Multiplexor::Multiplexor(string input1, string input2, int choice){
 
 }
 
-int Multiplexor::Multiplexor(int input1, int input2, int choice){
+int Multiplexer::useMultiplexer(int input1, int input2, int choice){
 	
 	if (debug){
-		cout << "*** MULTIPLEXOR INT/INT***" << "\n" << "Int1: " << input1 << " Int2: " << input2 << "Choice: " << choice << "\n";
+		cout << "*** MULTIPLEXER INT/INT***" << "\n" << "Int1: " << input1 << " Int2: " << input2 << "Choice: " << choice << "\n";
 	}
 	cout << "Input: " << input1 << " " << input2 << "\n"; 
 	
@@ -38,14 +41,14 @@ int Multiplexor::Multiplexor(int input1, int input2, int choice){
 	}
 }
 
-string Multiplexor::Multiplexor(string input1, int input2, int choice){
+string Multiplexer::useMultiplexer(string input1, int input2, int choice){
 
 	stringstream  ss;
 	ss<< input2;
 	string input2string = ss.str();
 	cout << "Input: " << input1 << " " << input2 << "\n"; 
 	if (debug){
-		cout << "*** MULTIPLEXOR STRING/INT***" << "\n" << "String1: " << input1 << " INT1: " << input2 << "Choice: " << choice << "\n";
+		cout << "*** MULTIPLEXER STRING/INT***" << "\n" << "String1: " << input1 << " INT1: " << input2 << "Choice: " << choice << "\n";
 	}
 	if(choice==0){
 		cout << "Output: " << input1;
