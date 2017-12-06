ac792b62be0a15fd43f9f2bf778c3cb6d27c10af
laurentoth <lauren.toth@richmond.edu>
2017-12-06 12:19:40 -0500

    Update main.cpp
    Added the use of the multiplexors

--- main.cpp
+++ main.cpp
@@ -22,11 +22,11 @@ int main (int argc, char *argv[]){
     ShiftLeft *shiftJump;
     ShiftLeft *shiftBranch;
     ControlUnit *control;
-    Multiplexer *registerMultiplexer1;
-    Multiplexer *registerOrImmediateMultiplexer2;
-    Multiplexer *memoryOrALUMultiplexer3;
-    Multiplexer *branchOrIncrementMultiplexer4;
-    Multiplexer *jumpOrIncrementMultiplexer5;
+    Multiplexer *registerMultiplexer1 = new Multiplexer();
+    Multiplexer *registerOrImmediateMultiplexer2 = new Multiplexer();
+    Multiplexer *memoryOrALUMultiplexer3 = new Multiplexer();
+    Multiplexer *branchOrIncrementMultiplexer4 = new Multiplexer();
+    Multiplexer *jumpOrIncrementMultiplexer5 = new Multiplexer();
     SignExtend *signExtend;
     BinaryOperation *BinaryOp;
     
@@ -153,7 +153,7 @@ int main (int argc, char *argv[]){
 	    control->setControls(instruction.substr(0,6);
 				 
 //Look to see if it is jump because will avoid all bottom stuff
-	if(control->getJump()==1){
+	if(control->getJump().compare("1")==0{
 	if(debug)
         cout <<"SETTING JUMP OR INCREMENTED ADDRESS INPUT0" << endl;
 
@@ -197,28 +197,32 @@ else{
 				 
         if (debug)
             cout << "ADJUSTING READ REGISTERS" << endl << endl;
-        if(control->getRegDST()==0){
-		registerFile->setWriteRegister(instruction.substr(11,5));
-	}
-	else{
-		registerFile->setWriteRegister(instruction.substr(16,5));
-	}
-      
+	
+	//Sets up the multiplexor that decides the write register
+	registerMultiplexor1->useMultiplexer(instruction.substr(11,5),instruction.substr(16,5),control->getRegDST());
+       	//Sets the write register based on the multiplexor
+	registerFile->setWriteRegister(registerMultiplexor1->getOutput());
+	      
     
     //Prepare the ALU inputs
 	 if (debug)
         cout <<"SETTING THE MEMORY ALU OPERANDS" << endl;
 	ALU1 = new ALU();
     	ALU1->setOperand1(registerFile->getReadRegister1());
-	if(control->getALUSrc()==0){
-		ALU1->setOperand2(registerFile->getReadRegister2());
-	}
-	else{
-		if (debug)
+	
+	//Does sign extend in case it is needed in the ALU
+	if (debug)
         	cout <<"SIGN EXTENDING IMMEDIATE" << endl << endl;
 		signExtend = new signExtend(instruction.substr(16,16);
 		ALU1->setOperand2(signExtend->getExtended());
-	}
+	
+	
+	registerOrImmediateMultiplexor2->useMultiplexor(registerFile->getReadRegister2(),signExtend->getExtended(),control->getALUSrc());
+	
+	ALU1->setOperand2(registerOrImmediateMultiplexor2->getOutput());
+	
+	
+		
 	ALUControl ALUcontrol = new ALUControl();
 	ALUcontrol->setALU(ALU1);
 	ALUcontrol->sendSignals(control->getALUOp());
@@ -235,29 +239,31 @@ else{
         cout <<"SETTING DATA MEMORY ADDRESS AND WRITE DATA" << endl;
 					    
 
-
-    if(control->getMemtoReg()==0){
+	memoryOrALUMultiplexor3->useMultiplexor(ALUresult,MemoryResult,control->getMemtoReg());
+					    
+    if(memoryOrALUMultiplexor->getOutput().compare(ALUresult)==0){
 	    //Need to write ALUresult to the writedata register
 	    int registerNum = BinaryOp->binToInt(registerFile->getWriteRegister());
-	    
 	    registerFile->writeToRegister(registerNum, BinaryOp->getHexFromBin(ALUresult));
 	    
 	    //ALL DONE WITH THE INSTRUCTION AT THIS POINT
+	    //NEED TO CHANGE PC
 	    
    	 }
 					    
 	else{
 			//Need to address the memory 
-	}
-					    
-   }					    
-
-    /*
+		 /*
     memoryUnit.setCurrentAddress(temp);
     temp = registerFile.getReadRegister2();
     memoryUnit.storeWord(temp);
     memoryUnit.saveMemory();
-    */				 
+    */	
+	}
+					    
+   }					    
+
+   			 
 	
 
     
