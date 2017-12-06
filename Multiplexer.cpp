6c3c23568162f1f509c3c3f5120649557493ae84
laurentoth <lauren.toth@richmond.edu>
2017-12-05 22:43:39 -0500

    Update Multiplexer.cpp

--- Multiplexer.cpp
+++ Multiplexer.cpp
@@ -3,59 +3,29 @@
 #include <sstream>
 
 Multiplexer::Mutiplexer(){
+	choice1="";
+	choice2="";
 }
 
-string Multiplexer::useMultiplexer(string input1, string input2, int choice){
+string Multiplexer::useMultiplexer(string input1, string input2, int muxChoice){
 	if (debug){
 		cout << "*** MULTIPLEXER STRING/STRING***" << "\n" << "String1: " << input1 << " String2: " << input2 << "Choice: " << choice << "\n";
 	}
 	
 	cout << "Input: " << input1 << " " << input2 << "\n"; 
 	
-	
+	choice1=input1;
+	choice2 = input2;
+	choice=muxChoice;
 	if(choice==0){
-		cout << "Output: " << input1;
-		return input1;
+		cout << "Output: " << choice1;
+		return choice1;
 	}
 	else{
-		cout << "Output: " << input2;
-		return input2;
+		cout << "Output: " << choice2;
+		return choice2;
 	}
 
 }
 
-int Multiplexer::useMultiplexer(int input1, int input2, int choice){
-	
-	if (debug){
-		cout << "*** MULTIPLEXER INT/INT***" << "\n" << "Int1: " << input1 << " Int2: " << input2 << "Choice: " << choice << "\n";
-	}
-	cout << "Input: " << input1 << " " << input2 << "\n"; 
-	
-	if(choice==0){
-		cout << "Output: " << input1;
-		return input1;
-	}
-	else{
-		cout << "Output: " << input2;
-		return input2;
-	}
-}
 
-string Multiplexer::useMultiplexer(string input1, int input2, int choice){
-
-	stringstream  ss;
-	ss<< input2;
-	string input2string = ss.str();
-	cout << "Input: " << input1 << " " << input2 << "\n"; 
-	if (debug){
-		cout << "*** MULTIPLEXER STRING/INT***" << "\n" << "String1: " << input1 << " INT1: " << input2 << "Choice: " << choice << "\n";
-	}
-	if(choice==0){
-		cout << "Output: " << input1;
-		return input1;
-	}
-	else {
-		cout << "Output: " << input2;
-		return input2string;
-	}
-}
