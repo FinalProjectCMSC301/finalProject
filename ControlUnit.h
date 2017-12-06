2b29fa45f0ff04509c4bf7c715dddafa8c07abe5
laurentoth <lauren.toth@richmond.edu>
2017-12-06 17:39:32 -0500

    Update ControlUnit.h

--- ControlUnit.h
+++ ControlUnit.h
@@ -15,7 +15,7 @@ class ControlUnit{
 public:
 	ControlUnit();
 	void setControls (string bitString);
-private:
+
 	string regDst;
 	string jump;
 	string branch;
@@ -47,8 +47,8 @@ private:
 	string setBranch(string b){ branch = b;};
 	string setMemRead(string mem){ memRead = mem;};
 	string setMemtoReg(string mem){ memtoReg = mem;};
-	string setALUOp1(string alu){ ALUOp1 = alu;};
-	string setALUOp0(string alu){ ALUOp0 = alu;};
+	string setAluOp1(string alu){ ALUOp1 = alu;};
+	string setAluOp0(string alu){ ALUOp0 = alu;};
 	string setMemWrite(string mem){ memWrite = mem;};
 	string setALUSrc(string alu){ ALUSrc = alu;};
 	string setRegWrite(string reg){ regWrite = reg;};
