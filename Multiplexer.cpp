637de5a82c0c6e93408e6372347435408d119f29
laurentoth <lauren.toth@richmond.edu>
2017-12-06 17:26:03 -0500

    Update Multiplexer.cpp

--- Multiplexer.cpp
+++ Multiplexer.cpp
@@ -3,21 +3,16 @@
 #include <sstream>
 
 Multiplexer::Mutiplexer(){
-	choice1="";
-	choice2="";
+	
 }
 
 void Multiplexer::useMultiplexer(string input1, string input2, string muxChoice){
-	if (debug){
-		cout << "*** MULTIPLEXER STRING/STRING***" << "\n" << "String1: " << input1 << " String2: " << input2 << "Choice: " << choice << "\n";
-	}
-	
-	cout << "Input: " << input1 << " " << input2 << "\n"; 
-	
+		
 	choice1=input1;
 	choice2 = input2;
 	choice=muxChoice;
-	if(choice==0){
+	
+	if(choice.compare("0")==0){
 		cout << "Output: " << choice1;
 		output = choice1;
 	}
