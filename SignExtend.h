f7da276df011b4fbd13f943686e6882e225a7808
rishabhjaintr <rishabhjaintr@gmail.com>
2017-12-07 16:35:16 -0500

    Commenting done.

--- SignExtend.h
+++ SignExtend.h
@@ -5,13 +5,19 @@
 #include "InstructionMemory.h" //perhaps?
 #include <iostream>
 
+/** This class is used as the sign extended unit.
+*/
 class SignExtend
 {
 	public:
+		/** Takes in a address of 16 bits.
+		*/
 		SignExtend(string address);//this is in binary
 			//instruction would pass bits to this in string
 			//which should technically be 16 bits in length
 			//but I don't think it's necessary to check
+		/** Returns a string that is extended and is not 32 bits.
+		*/
 		string getExtended();
 		
 		private:
