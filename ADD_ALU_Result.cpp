091c28348ef524d7dd1176fde4fcbc3cf4209f0c
rishabhjaintr <rishabhjaintr@gmail.com>
2017-12-07 15:34:15 -0500

    Update ADD_ALU_Result.cpp

--- ADD_ALU_Result.cpp
+++ ADD_ALU_Result.cpp
@@ -2,22 +2,27 @@
 #include "ADD_ALU_Result.h"
 using namespace std;
 
+/** Default Constructor
+*/
 ADD_ALU_Result::ADD_ALU_Result()
 {
 	alu2Bin32 = "";
 	shiftBin32 = "";
 }
-
+/** Used to set input to this ALU from ADD.
+*/
 void ADD_ALU_Result::setAlu2Input(string bin32)
 {
 	alu2Bin32 = bin32;
 }
-
+/** Used to set input from Shift left.
+*/
 void ADD_ALU_Result::setShiftInput(string bin32)
 {
 	shiftBin32 = bin32;
 }
-
+/** returns a string with an addition of two inputs in binary
+*/
 string ADD_ALU_Result::getBinaryOutput()
 {
 	//simple add
