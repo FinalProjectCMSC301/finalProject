faa2b512afa97fc4f67b8d60a1164292268bc049
rishabhjaintr <rishabhjaintr@gmail.com>
2017-12-07 16:31:56 -0500

    Commenting done.

--- ShiftLeft.h
+++ ShiftLeft.h
@@ -4,14 +4,15 @@
 #include <string>
 using namespace std;
 
+/** General class to Shift left. 
+*/
 class ShiftLeft
 {
 	public:
+		/** Default constructor.
+		*/
 		ShiftLeft();
-		
-		/*
-			The method shifts the 26 bit binary number string left by 2 and adds 2 additional
-			bits. The method returns the modifed 28-bit binary string.
+		/** Adds "00" at the end of input and returns it.
 		*/
 		string shift(string input);
 };
