a992fc0e549b54805bf3dc8294d8c48ac6baf664
rishabhjaintr <rishabhjaintr@gmail.com>
2017-12-07 15:43:31 -0500

    Commenting done.

--- ALU_ALU_Result.h
+++ ALU_ALU_Result.h
@@ -6,9 +6,14 @@
 class ALU_ALU_Result
 {
 	private:
+		/** checks are used to see that all three required inputs have been received and after execute()
+		is done, they are reset so that the same numbers do not execute.
+		*/
 		int check1;
 		int check2;
 		int check3;
+		/** Used when conversion or addion between int-bin-hex is needed.
+		*/
 		BinaryOperation bo;
 		string branchBit;
 		unsigned int regData;
@@ -17,14 +22,32 @@ class ALU_ALU_Result
 		unsigned int result;
 		
 	public:
+		/** Default constructor.
+		*/
 		ALU_ALU_Result();
+		/** Used to set a 3 bit input from ALU Control unit.
+		*/
 		void setAluControlInput(string operation);
+		/** Used to set data from register file.
+		*/
 		void setDataFromReg(string hex);
+		/** Used to set data from the multiplexer
+		*/
 		void setDataFromMux(string data); //can be hex from reg, or binary from imm
+		/** Used to do the required operation on the given operands.
+		*/
 		void execute();
+		/** Used to get the result of execute() in binary.
+		*/
 		string getBinaryOutput();
+		/** Used to get the result of execute() in hex.
+		*/
 		string getHexOutput();
+		/** Used to see the control signal to AND unit which selectes if branch should be done.
+		*/
 		string getBranchBit();
+		/** Used to set the branch bit to AND unit.
+		*/
 		void setBranchBit(string bit1);
 };
 #endif
