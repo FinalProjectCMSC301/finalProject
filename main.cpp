ea321f20ba6025c0e496baecfb33ac17b0d9d3c9
laurentoth <lauren.toth@richmond.edu>
2017-12-07 04:54:19 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -198,7 +198,6 @@ else{
 	cout << "Register data" << endl; 
 	registerFile->setRead(1);
 		cout << BinaryOp->hexToInt(registerFile->read(num))<< "   " << registerFile->read(num)<< endl;
-		cout << BinaryOp->hexToInt(registerFile->read(num2))<< "   " << registerFile->read(num2)<< endl;
 		registerFile->setRead(0);
 	cout << "Executed ALU. Result: " << BinaryOp->binToInt(alu1->getOutput()) << endl;
 	string ALUresult = BinaryOp->binToHex(alu1->getOutput(),8);
