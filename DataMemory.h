9080865ed2ea58d77e199410057d97bcc325ca08
rishabhjaintr <rishabhjaintr@gmail.com>
2017-12-07 16:16:47 -0500

    Commenting done.

--- DataMemory.h
+++ DataMemory.h
@@ -14,6 +14,8 @@
 
 using namespace std;
 
+/** Representation of data memory.
+*/
 class DataMemory
 {
 	private:
@@ -24,23 +26,35 @@ class DataMemory
 		
 	public:
 		//This takes the file name which contains the memory input and loads every address:value
+		/** Constructor takes in a filename, and a bool that decides if debug values will be displayed.
+		*/
 		DataMemory(string file, bool debug);
 		
 		//sw $t1, 0($t2)
 		//writes to a particular address
 		//address in HEX WITH a 0x
+		/** Given a hex string address, writes the given values (which should be in hex as well).
+		*/
 		void writeToMemory(string address, string value);
 		
 		//control signal that tells if memory CAN be overwritten
+		/** Decides whether memory can be written.
+		*/
 		void setWrite(bool writeBool);
 		
 		//control signal that tells if memory should be read for a particular instruction
+		/** Decides whether memory can be read.
+		*/
 		void setRead(bool readBool);
 		
 		//given a particular hex address, what does that address contain?
+		/** Given a hex address, returns the associated hex string.
+		*/
 		string read(string address);
 		
 		//prints all memory
+		/** Returns a string that contains whole data memory.
+		*/
 		string print();
 		
 		//this is to print debug info
