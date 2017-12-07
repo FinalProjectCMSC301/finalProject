db7e63ccb56fa58ca015eae33c06e0d927c69809
laurentoth <lauren.toth@richmond.edu>
2017-12-06 22:04:14 -0500

    Update ShiftLeft.cpp

--- ShiftLeft.cpp
+++ ShiftLeft.cpp
@@ -2,12 +2,6 @@
 
 using namespace std;
 
-// Default constructor
-ShiftLeft::ShiftLeft()
-{
-
-}
-
 /*
 The method shifts the 26 bit binary number string left by 2 and adds 2 additional
 bits. The method returns the modifed 28-bit binary string.
