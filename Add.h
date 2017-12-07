7983ebed7d878f78e344aa5ca0d7bcd1c697881e
rishabhjaintr <rishabhjaintr@gmail.com>
2017-12-07 15:46:40 -0500

    Commenting done.

--- Add.h
+++ Add.h
@@ -4,17 +4,28 @@
 #include "BinaryOperation.h"
 
 using namespace std;
-
+/** Class name was ADD before but it gave errors for some reason which went away after name changed to Add.
+*/
 class Add
 {
 	private:
 		BinaryOperation bo;
 		string hexPC;
 	public:
+		/** Default constructor.
+		*/
 		Add();
+		/** Requires a hex value of PC.
+		*/
 		void setPCInput(string hex);
+		/** When called, 4 is added to the PC given earlier.
+		*/
 		void update();
+		/** Can be used to get the binary rep. of PC.
+		*/
 		string getBinaryOutput();
+		/** Can be used to get the hex rep. of PC
+		*/
 		string getHexOutput();
 };
 #endif
